53a7dd20a7bad58d4efeea3fc2da4faedf8b05ba
Narciso Sousa <=>
2016-12-06 01:43:50 -0300

    CRUD de Client

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    public function index(){
+        return Client::all();
+    }
+
+    public function storage(Request $request){
+        return Client::create($request->all());
+    }
+
+    public function show($id){
+        return Client::find($id);
+    }
+
+    public function destroy($id){
+        Client::find($id)->delete();
+    }
+    public function update(Request $request, $id){
+       Client::find($id)->update($request->all());
+        return $this->show($id);
+    }
+
+}
